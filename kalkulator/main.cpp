ba4bcb67d3e905fe455a4ebd56a4306580243a5f
Milossh_ <89982638+hozakowskimilosz@users.noreply.github.com>
2021-09-09 16:40:55 +0200

    Add files via upload

---
+++ kalkulator/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+
+class Rectangle {
+	public:
+		float a, b;
+		float pole(float a, float b);
+		void showPole();
+};
+
+float Rectangle::pole(float a, float b){
+	return a*b;
+}
+
+int main(int argc, char** argv) {
+	setlocale(LC_CTYPE, "polish");
+	
+	Rectangle prostok¹t;
+	prostok¹t.
+	
+	
+		
+		
+	
+
+	return 0;
+}
