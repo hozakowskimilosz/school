4b2b70b5d1de0a512d01f7e0cff098e248f1de97
Milossh_ <89982638+hozakowskimilosz@users.noreply.github.com>
2021-09-09 16:13:21 +0200

    Add files via upload

---
+++ OPP 09.09/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class Test {
+	public:
+		int a;
+};
+
+class Worker {
+	public:
+		string name;
+		string surname;
+		string nationality;
+		unsigned short int yearBirthday;
+		float height;
+		char gender;					
+	void showNameSurname(){
+		cout << "Imiê i nazwisko: " << name << " " <<surname;	
+	};
+	
+	void showAllData();				
+};
+
+	void Worker::showAllData(){
+		
+		string genderDescription;
+		switch(gender){
+			case 'M':
+				genderDescription = "Mê¿czyzna";
+				break;
+			case 'W':
+				genderDescription = "Kobieta";
+				break;
+			default:
+				genderDescription = "-";
+	    }
+	    
+		cout << "\n\n\Imiê i nazwisko: " << name << " " << surname << "\n"
+		<< "\n\Narodowoœæ: " << nationality << "\n\nrok urodzenia: "
+		<< yearBirthday << "\n\nwzrost: " << height << "\n\np³eæ: "
+		<< genderDescription;
+	}
+
+int main(int argc, char** argv) {
+	setlocale(LC_CTYPE, "polish");
+	Test newObject;
+//	cout << newObject.a;
+
+	Worker pracownik;
+	pracownik.name="Jan";
+	pracownik.surname="Kowalski";
+	pracownik.nationality="Polska";
+	pracownik.yearBirthday=1998;
+	pracownik.height=180;
+	pracownik.gender='M';
+	cout << pracownik.name << endl << endl;
+	pracownik.showNameSurname();
+	pracownik.showAllData();
+	
+	return 0;
+}
+
+
+
+
