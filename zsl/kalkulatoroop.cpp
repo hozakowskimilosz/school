ad957cf6530044696621b0ee310a3ce0c5039685
Milossh_ <89982638+hozakowskimilosz@users.noreply.github.com>
2021-09-15 19:36:29 +0200

    Add files via upload

---
+++ zsl/kalkulatoroop.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+	public:
+		float bok1;
+		float bok2;
+		
+		float pole() {
+			return bok1*bok2;
+		}
+		
+		float obwod() {
+			return bok1*2+bok2*2;
+		}
+		
+		void showValues(float pole, float obwod) {
+			cout << "Pole prostok¹tu to " << pole << ", a jego obwód wynosi " << obwod;
+		}
+};
+
+int main() {
+	setlocale(LC_CTYPE,"Polish");
+	
+	float a;
+	float b;
+
+	cout << "Podaj d³ugoœæ 1 boku: ";
+	cin >> a;
+	cout << "Podaj d³ugoœæ 2 boku: ";
+	cin >> b;
+		
+	Rectangle obj;
+		obj.bok1 = a;
+		obj.bok2 = b;
+		
+	obj.showValues(obj.pole(), obj.obwod());
+		
+	return 0;
+}
